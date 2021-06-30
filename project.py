4f32c64a74f3076f9dfc9fcb22130c7b4c902aa2
Nathaniel Beversluis <nathanielbeversluis@Nathaniels-MacBook-Air.local>
2021-06-30 11:25:31 -0400

    restore async

--- project.py
+++ project.py
@@ -140,7 +140,7 @@ class UrtextProject:
                  init_project=False):
         
         self.is_async = True 
-        self.is_async = False # development only
+        #self.is_async = False # development only
         self.path = path
         self.reset_settings()
         self.nodes = {}
